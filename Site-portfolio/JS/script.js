f8550e9a4b5e1fee9aaa2f72346556c09497037a
Narkes Zholymova <nzholymova@gmail.com>
2019-07-02 22:22:41 +0600

    Portfolio

---
+++ Site-portfolio/JS/script.js
@@ -0,0 +1,11 @@
+menu.onclick= funcion myFunction() {
+var x = document.getElementById("myTopnav");
+if(x.className === "topnav") {
+	x.className+=" responsive ";
+	
+}
+
+else {
+	x.className=" topnav";
+}
+}
